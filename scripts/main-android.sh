73f9aed82d3e4072929a6eeaead0e459bb328035
Taner Sener <tanersener@gmail.com>
2026-07-27 00:18:47 +0300

    dont depend on libiconv for lame since we dont build lame cli

--- scripts/main-android.sh
+++ scripts/main-android.sh
@@ -73,11 +73,6 @@ while [ ${#enabled_library_list[@]} -gt $completed ]; do
         run=1
       fi
       ;;
-    lame)
-      if [[ $OK_libiconv -eq 1 ]]; then
-        run=1
-      fi
-      ;;
     leptonica)
       if [[ $OK_giflib -eq 1 ]] && [[ $OK_jpeg -eq 1 ]] && [[ $OK_libpng -eq 1 ]] && [[ $OK_tiff -eq 1 ]] && [[ $OK_libwebp -eq 1 ]]; then
         run=1
